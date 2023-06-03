de5cf8589d83a7dc10f9ec77bc1b253f1fe9e53c
Maciej Bak <wsciekly.maciek@gmail.com>
2023-06-03 11:36:22 +0200

    test

--- ToySnake/test.cpp
+++ ToySnake/test.cpp
@@ -17,9 +17,11 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/eigen.h>
+#include <pybind11/pytypes.h>
 #include <Eigen/LU>
 #include <carma>
 #include <armadillo>
+
 namespace py = pybind11;
 
 double add2numbers(double i, double j) {
@@ -33,6 +35,26 @@ double square_a_number(double x) {
 Eigen::MatrixXd inverse(Eigen::MatrixXd m) {
     return m.inverse();
 }
+
+// https://carma.readthedocs.io/en/latest/introduction.html#ols-example
+py::tuple ols(arma::mat& X, arma::colvec& y) {
+    // We borrow the data underlying the numpy arrays
+    int n = X.n_rows, k = X.n_cols;
+
+    arma::colvec coeffs = arma::solve(X, y);
+    arma::colvec resid = y - X * coeffs;
+
+    double sig2 = arma::as_scalar(arma::trans(resid) * resid / (n-k));
+    arma::colvec std_errs = arma::sqrt(sig2 * arma::diagvec( arma::inv(arma::trans(X)*X)) );
+
+    // We take ownership of the memory from the armadillo objects and
+    // return to python as a tuple containing two Numpy arrays.
+    return py::make_tuple(
+        carma::col_to_arr(coeffs),
+        carma::col_to_arr(std_errs)
+    );
+}
+// adapted from https://gallery.rcpp.org/articles/fast-linear-model-with-armadillo/
 
 // binding C++ code to Python
 PYBIND11_MODULE(functions, m) {
