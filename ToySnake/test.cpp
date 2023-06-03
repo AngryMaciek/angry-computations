99005dde442c2e1721488f30aac8f9af51356b6a
Maciek Bąk <wsciekly.maciek@gmail.com>
2023-06-03 13:45:26 +0200

    refactor: add github codespaces (#5)

--- ToySnake/test.cpp
+++ ToySnake/test.cpp
@@ -18,7 +18,7 @@
 #include <pybind11/numpy.h>
 #include <pybind11/eigen.h>
 #include <pybind11/pytypes.h>
-#include <Eigen/LU>
+#include <Eigen/LU> // for inverse
 #include <carma>
 #include <armadillo>
 
