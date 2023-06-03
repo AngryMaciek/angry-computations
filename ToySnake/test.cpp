86ec5672add8a89537940561736bb3bd106be774
Maciej Bak <wsciekly.maciek@gmail.com>
2023-06-03 12:13:37 +0200

    test

--- ToySnake/test.cpp
+++ ToySnake/test.cpp
@@ -37,7 +37,7 @@ Eigen::MatrixXd inverse(Eigen::MatrixXd m) {
 }
 
 // https://carma.readthedocs.io/en/latest/introduction.html#ols-example
-py::tuple lm(arma::mat& X, arma::mat& y) {
+py::tuple lm(arma::mat& X, arma::colvec& y) {
     // We borrow the data underlying the numpy arrays
     int n = X.n_rows, k = X.n_cols;
 
@@ -46,13 +46,13 @@ py::tuple lm(arma::mat& X, arma::mat& y) {
 
     double sig2 = arma::as_scalar(arma::trans(resid) * resid / (n-k));
     //arma::colvec std_errs = arma::sqrt(sig2 * arma::diagvec( arma::inv(arma::trans(X)*X)) );
-    arma::mat std_errs = arma::inv(arma::trans(X)*X);
+    arma::colvec std_errs = arma::diagvec(arma::inv(arma::trans(X)*X));
 
     // We take ownership of the memory from the armadillo objects and
     // return to python as a tuple containing two Numpy arrays.
     return py::make_tuple(
-        carma::col_to_arr(coeffs),
-        carma::mat_to_arr(std_errs)
+        carma::col_to_arr(coeffs), // use mat_to_arr() to return arma::mat
+        carma::col_to_arr(std_errs)
     );
 }
 // adapted from https://gallery.rcpp.org/articles/fast-linear-model-with-armadillo/
